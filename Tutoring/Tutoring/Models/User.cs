5211e43c0da8683855c0b85cad01c7f70c97bdf7
Fifok <fifok77@gmail.com>
2021-05-13 23:03:21 +0200

    Update User model (add Age and Fullname properties)

--- Tutoring/Tutoring/Models/User.cs
+++ Tutoring/Tutoring/Models/User.cs
@@ -13,5 +13,21 @@ namespace Tutoring.Models
         public string Email { get; set; }
         public string Password { get; set; }
         public DateTime Birthdate { get; set; }
+        public int Age
+        {
+            get
+            {
+                var age = DateTime.Today.Year - Birthdate.Year;
+                if (Birthdate.Date > DateTime.Today.AddYears(-age)) age--;
+                return age;
+            }
+        }
+        public string Fullname
+        {
+            get
+            {
+                return $"{Firstname} {Lastname}";
+            }
+        }
     }
 }
