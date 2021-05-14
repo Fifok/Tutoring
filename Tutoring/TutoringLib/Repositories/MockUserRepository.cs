be3776b956a1bfbb6717d5159f2a5b3288a77cbe
Fifok <fifok77@gmail.com>
2021-05-14 09:11:00 +0200

    Add MockUserRepository

---
+++ Tutoring/TutoringLib/Repositories/MockUserRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutoring.Models;
+
+namespace TutoringLib.Repositories
+{
+    public class MockUserRepository : IRepository<User>
+    {
+        public void Add(User model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(User model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
