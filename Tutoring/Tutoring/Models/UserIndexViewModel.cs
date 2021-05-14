311d7058416e4b7b6c1b9ca3b400ec1dc585fbae
Fifok <fifok77@gmail.com>
2021-05-14 15:56:31 +0200

    Add UserIndexViewModel

---
+++ Tutoring/Tutoring/Models/UserIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tutoring.Models
+{
+    public class UserIndexViewModel
+    {
+        public string Fullname { get; set; }
+        public DateTime Birthdate { get; set; }
+        public int Age { get; set; }
+    }
+}
