658d542666f2fadc9c9eeb4ead7098f687f69453
Fifok <fifok77@gmail.com>
2021-05-14 09:04:43 +0200

    Add IRepository interface

---
+++ Tutoring/TutoringLib/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TutoringLib.Repositories
+{
+   
+    public interface IRepository<T>
+    {
+        T GetById(int id);
+        T GetAll();
+        void Add(T model);
+        void Remove(T model);
+    }
+}
