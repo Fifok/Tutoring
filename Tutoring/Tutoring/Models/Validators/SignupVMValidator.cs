df811644aae7f969d8fe31fa9959d75ff70b5d00
Fifok <fifok77@gmail.com>
2021-05-19 15:15:24 +0200

    Add SignupVMValidator

---
+++ Tutoring/Tutoring/Models/Validators/SignupVMValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tutoring.Models.Validators
+{
+    public class SignupVMValidator : AbstractValidator<SignupViewModel>
+    {
+        public SignupVMValidator()
+        {
+            RuleFor(x => x.Firstname).NotEmpty();
+            RuleFor(x => x.Lastname).NotEmpty();
+            RuleFor(x => x.Email).NotEmpty().EmailAddress();
+            RuleFor(x => x.Password).NotEmpty();
+        }
+    }
+}
