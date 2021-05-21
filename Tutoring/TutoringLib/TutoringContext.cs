fb2e8be9abc32188e8fc4746d41093fc0b5fbee2
Fifok <fifok77@gmail.com>
2021-05-21 22:39:16 +0200

    Add TutoringContext

---
+++ Tutoring/TutoringLib/TutoringContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TutoringLib.Models;
+
+namespace TutoringLib
+{
+    public class TutoringContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Tutorial> Tutorials { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public TutoringContext(DbContextOptions<TutoringContext> options) : base(options)
+        {
+        }
+    }
+}
