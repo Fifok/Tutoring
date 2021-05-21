22db7a2c0217ca56f7bbb5b7fd784267ed694fd3
Fifok <fifok77@gmail.com>
2021-05-21 22:26:52 +0200

    Add Comment model

---
+++ Tutoring/TutoringLib/Models/Comment.cs
@@ -0,0 +1,12 @@
+﻿namespace TutoringLib.Models
+{
+    public class Comment
+    {
+        public int UserId { get; set; }
+        public virtual User Author { get; set; }
+        public int PageId { get; set; }
+        public virtual Page Page { get; set; }
+        public string Content { get; set; }
+
+    }
+}
