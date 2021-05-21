f3978fdb1f077c97c988afa265e874f8601f0343
Fifok <fifok77@gmail.com>
2021-05-21 23:00:34 +0200

    Move Db items to Tutoring project

---
+++ Tutoring/Tutoring/Models/Db/Models/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TutoringLib.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Nickname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime Birthdate { get; set; }
+        public int Age
+        {
+            get
+            {
+                var age = DateTime.Today.Year - Birthdate.Year;
+                if (Birthdate.Date > DateTime.Today.AddYears(-age)) age--;
+                return age;
+            }
+        }
+        public string Fullname
+        {
+            get
+            {
+                return $"{Firstname} {Lastname}";
+            }
+        }
+
+        public virtual ICollection<Tutorial> CreatedTutorials { get; set; }
+    }
+}
---
+++ Tutoring/Tutoring/Models/Db/TutoringContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TutoringLib.Models;
+
+namespace TutoringLib
+{
+    public class TutoringContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Tutorial> Tutorials { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public TutoringContext(DbContextOptions<TutoringContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>().Property(x => x.Firstname).IsRequired();
+            modelBuilder.Entity<User>().Property(x => x.Lastname).IsRequired();
+            modelBuilder.Entity<User>().Property(x => x.Email).IsRequired();
+            modelBuilder.Entity<User>().Property(x => x.Password).IsRequired();
+
+            modelBuilder.Entity<Tutorial>().Property(x => x.Title).IsRequired().HasMaxLength(256);
+
+            modelBuilder.Entity<User>().HasMany(x => x.CreatedTutorials)
+                .WithOne(x => x.Author)
+                .HasForeignKey(x => x.AuthorId);
+
+        }
+    }
+}
