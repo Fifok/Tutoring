523cba8238d799ed4a70b8437a2dee95c21bff63
Fifok <fifok77@gmail.com>
2021-05-21 22:49:15 +0200

    Add AuthorId property to Tutorial model

--- Tutoring/TutoringLib/Models/Tutorial.cs
+++ Tutoring/TutoringLib/Models/Tutorial.cs
@@ -11,6 +11,7 @@ namespace TutoringLib.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public int AuthorId { get; set; }
         public User Author { get; set; }
         public ICollection<string> Tags { get; set; }
         public ICollection<Page> Pages { get; set; }
