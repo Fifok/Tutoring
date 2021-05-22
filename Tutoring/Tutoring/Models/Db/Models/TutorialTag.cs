e1f7538bc783bcede58f07754224411e89e354c4
Fifok <fifok77@gmail.com>
2021-05-22 21:51:42 +0200

    Add TutorialTag

---
+++ Tutoring/Tutoring/Models/Db/Models/TutorialTag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tutoring.Models.Db.Models
+{
+    public class TutorialTag
+    {
+        public int TutorialId { get; set; }
+        public Tutorial Tutorial { get; set; }
+        public int TagId { get; set; }
+        public Tag Tag { get; set; }
+    }
+}
