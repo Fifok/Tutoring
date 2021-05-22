82f90f121d97eac0d1929d26fc53a041f0a2eb8e
Fifok <fifok77@gmail.com>
2021-05-22 22:14:50 +0200

    Add ContentItem model

---
+++ Tutoring/Tutoring/Models/Db/Models/ContentItem.cs
@@ -0,0 +1,10 @@
+﻿namespace Tutoring.Models.Db.Models
+{
+    public class ContentItem
+    {
+        public int PageId { get; set; }
+        public Page Page { get; set; }
+        public string Content { get; set; }
+
+    }
+}
