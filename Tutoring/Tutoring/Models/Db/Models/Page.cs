62e7c6ed97db96dfc2292e9e13dc1a284bca64a6
Fifok <fifok77@gmail.com>
2021-05-22 22:15:15 +0200

    Change type of Content

--- Tutoring/Tutoring/Models/Db/Models/Page.cs
+++ Tutoring/Tutoring/Models/Db/Models/Page.cs
@@ -10,7 +10,7 @@ namespace Tutoring.Models.Db.Models
         public int TutorialId { get; set; }
         public virtual Tutorial Tutorial { get; set; }
         public string Title { get; set; }
-        public virtual ICollection<string> Content { get; set; }
+        public virtual ICollection<ContentItem> Content { get; set; }
         public virtual ICollection<Comment> Comments { get; set; }
     }
 }
