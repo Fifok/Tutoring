123dcd35e879fa4ff9d193cc78c6ce4853ca3891
Fifok <fifok77@gmail.com>
2021-05-23 18:43:50 +0200

    Add TutorialIndexViewModel

---
+++ Tutoring/Tutoring/Models/TutorialIndexViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tutoring.Models
+{
+    public class TutorialIndexViewModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public UserInfoViewModel Author { get; set; }
+        public ICollection<PageViewModel> Pages { get; set; }
+
+    }
+}
