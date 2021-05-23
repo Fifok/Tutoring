c617d5f7523902e3ac09478de52ea7452cd5d7d0
Fifok <fifok77@gmail.com>
2021-05-23 18:43:36 +0200

    Add PageViewModel

---
+++ Tutoring/Tutoring/Models/PageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Tutoring.Models.Db.Models;
+
+namespace Tutoring.Models
+{
+    public class PageViewModel
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
