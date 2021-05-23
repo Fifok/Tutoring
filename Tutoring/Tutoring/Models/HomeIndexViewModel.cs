036a8fa2f3068b9b71a084f659722af1d66935f0
Fifok <fifok77@gmail.com>
2021-05-23 16:47:53 +0200

    Add TutorialIndexViewModel

---
+++ Tutoring/Tutoring/Models/HomeIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tutoring.Models
+{
+    public class HomeIndexViewModel
+    {
+        public ICollection<TutorialListItemViewModel> Tutorials { get; set; }
+    }
+}
