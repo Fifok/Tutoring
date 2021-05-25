d1e0da8b25728a50e77174cb5394b57726ed9cb3
Fifok <fifok77@gmail.com>
2021-05-25 18:49:37 +0200

    Add TutorialAddViewModel

---
+++ Tutoring/Tutoring/Models/TutorialM/TutorialAddViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tutoring.Models.TutorialM
+{
+    public class TutorialAddViewModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public ICollection<PageViewModel> Pages { get; set; }
+    }
+}
