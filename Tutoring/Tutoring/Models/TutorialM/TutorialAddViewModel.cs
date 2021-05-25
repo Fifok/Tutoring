6d062ee381db4565188ccfc800e5cdae658ce33f
Fifok <fifok77@gmail.com>
2021-05-25 21:30:42 +0200

    Remove Pages property

--- Tutoring/Tutoring/Models/TutorialM/TutorialAddViewModel.cs
+++ Tutoring/Tutoring/Models/TutorialM/TutorialAddViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,6 +10,5 @@ namespace Tutoring.Models.TutorialM
     {
         public string Title { get; set; }
         public string Description { get; set; }
-        public ICollection<PageViewModel> Pages { get; set; }
     }
 }
